3fd9eccd68a0ce192119e922b9ed28bfecca6b3d
Hieu Nguyen <hieutnguyen@FVFXL1ELHV2H.local>
2018-11-27 23:14:05 +0700

    init file: add library: react, redux. add static check: flow, eslint

---
+++ src/components/App.js
@@ -0,0 +1,9 @@
+// @flow
+import React, { Component } from 'react';
+
+type typeProps = {};
+export default class App extends Component<typeProps> {
+  render() {
+    return <h1>Hello there World!</h1>;
+  }
+}
