28de6182f77042e3a9ee39097ded3aa2837ce0c6
HieuNguyen <mr.leexung@gmail.com>
2018-12-10 22:26:40 +0700

    commit 1

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@
 import React, { Component } from 'react';
 import Input from './Input';
 type typeProps = {};
+// commit 1
 export default class App extends Component<typeProps> {
   render() {
     return (
